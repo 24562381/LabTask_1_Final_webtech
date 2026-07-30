033fdcf6c8b3b49c3dfce5bdb7172025d473c172
Sajedul Islam Sakib <246383455+sajedulisakib-001@users.noreply.github.com>
2026-07-30 17:55:05 +0600

    a

--- index.php
+++ index.php
@@ -47,66 +47,76 @@
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <style>
-        #id{
-            display: flex;
-            justify-content: center;
-        }
+    #id {
+        display: flex;
+        justify-content: center;
+    }
     </style>
 </head>
+
 <body>
-        <h1>Manu:</h1>
-       <table border>
-            <tr>
-                <th>Item No</th>
-                <th>Name</th>
-                <th>Price</th>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>Burger</td>
-                <td>$5</td>
-            </tr>
-            <tr>
-                <td>2</td>
-                <td>Pizza</td>
-                <td>$8</td>
-            </tr>
-            <tr>
-                <td>3</td>
-                <td>Sandwitch</td>
-                <td>$4</td>
-            </tr>
-            <tr>
-                <td>4</td>
-                <td>Coffie</td>
-                <td>$3</td>
-            </tr>
-        </table>
+    <h1>Manu:</h1>
+    <table border>
+        <tr>
+            <th>Item No</th>
+            <th>Name</th>
+            <th>Price</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Burger</td>
+            <td>$5</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Pizza</td>
+            <td>$8</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Sandwitch</td>
+            <td>$4</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Coffie</td>
+            <td>$3</td>
+        </tr>
+    </table>
 
-        <h1>Customer Info & Order:</h1>
-        <div id="form">
-            <form method="post" >
-            <label for="name">Name:</label>
-            <input type="text" id="name" name="name" required placeholder="Please enter your name">
+    <h1>Customer Info & Order:</h1>
+    <div id="form">
+        <form method="post">
+            <div>
+                <label for="name">Name:</label>
+                <input type="text" id="name" name="name" required placeholder="Please enter your name">
 
-            <label for="id">ID:</label>
-            <input type="text" id="id" name="id" required placeholder="Please enter your id">
-
-            <label for="id">ID:</label>
-            <input type="text" id="id" name="id" required placeholder="Please enter your id">
-
+            </div>
+            <div>
+                <label for="id">ID:</label>
+                <input type="text" id="id" name="id" required placeholder="Please enter your id">
+            </div>
+            <div>
+                <label for="id">ID:</label>
+                <input type="text" id="id" name="id" required placeholder="Please enter your id">
+            </div>
             <label for="item">Manu:</label>
-            <select id="item" name="id" required >
+            <select id="item" name="id" required>
                 <option value="0">Please Select an item</option>
                 <option value="1">Burger</option>
                 <option value="2">Pizza</option>
                 <option value="3">Sandwitch</option>
                 <option value="4">Coffie</option>
             </select>
+            <div>
+                <lable for="count">Select Quntity:</lable>
+                <input type="number" id="quantity" name="quantity" value=1>
+            </div>
         </form>
-        </div>
+    </div>
 </body>
 
 
